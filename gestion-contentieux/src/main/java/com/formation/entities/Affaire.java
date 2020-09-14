4a6bd404380325d90b7d5abb43c559c25fd2c3d6
MarionGl <marion.gloriant@gmail.com>
2020-09-14 17:29:07 +0200

    modif classe Affaire

--- gestion-contentieux/src/main/java/com/formation/entities/Affaire.java
+++ gestion-contentieux/src/main/java/com/formation/entities/Affaire.java
@@ -12,4 +12,27 @@ public class Affaire implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private Long idAffaire;
+	private String reference;
+	private String titre;
+	private String description;
+	private int statut;
+	
+	
+	public Affaire() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Affaire(Long idAffaire, String reference, String titre, String description, int statut) {
+		super();
+		this.idAffaire = idAffaire;
+		this.reference = reference;
+		this.titre = titre;
+		this.description = description;
+		this.statut = statut;
+	}
+	
+	
 }
