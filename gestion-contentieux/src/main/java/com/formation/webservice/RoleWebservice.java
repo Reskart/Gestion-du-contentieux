0213cb61f15f47f2ebafa84e886ae284b8004d51
Reskart <antoinebertinb@gmail.com>
2020-09-15 14:12:50 +0200

    ajout role webservice

--- gestion-contentieux/src/main/java/com/formation/webservice/RoleWebservice.java
+++ gestion-contentieux/src/main/java/com/formation/webservice/RoleWebservice.java
@@ -1,7 +1,10 @@
 package com.formation.webservice;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +26,24 @@ public class RoleWebservice {
 		return service.saveOrUpdate(r);
 	}
 	
+	@RequestMapping(value="/update", method=RequestMethod.PUT)
+	public Role update(@RequestBody Role r) {
+		return service.saveOrUpdate(r);
+	}
 	
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.DELETE)
+	public void delete(@PathVariable Long id) {
+		service.delete(id);
+	}
+	
+	@RequestMapping(value="/role/{id}", method=RequestMethod.GET)
+	public Role getById(Long id) {
+		return service.getById(id);
+	}
+	
+	@RequestMapping(value="/roles", method=RequestMethod.GET)
+	public List<Role> getAll(){
+		return service.getAll();
+	}
 	
 }
