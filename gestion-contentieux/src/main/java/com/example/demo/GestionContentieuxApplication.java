20f9513e93e90602ec7f76162baaadd9a00dce8d
Benjamin Thomas <b-thomas@outlook.fr>
2020-09-15 15:45:40 +0200

    correction base package

--- gestion-contentieux/src/main/java/com/example/demo/GestionContentieuxApplication.java
+++ gestion-contentieux/src/main/java/com/example/demo/GestionContentieuxApplication.java
@@ -18,7 +18,7 @@ public class GestionContentieuxApplication {
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors
-						.basePackage("com.inti.formation")).build();
+						.basePackage("com.formation")).build();
 
 
 	}
