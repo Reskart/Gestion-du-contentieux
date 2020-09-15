5aba4e86df438e2f160416637829870bab46e259
Benjamin Thomas <b-thomas@outlook.fr>
2020-09-15 14:38:34 +0200

    Ajout Service WebService Tribunal

---
+++ gestion-contentieux/src/main/java/com/formation/service/impl/TribunalService.java
@@ -0,0 +1,40 @@
+package com.formation.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.formation.entities.Tribunal;
+import com.formation.repositories.ITribunalRepository;
+import com.formation.service.ITribunalService;
+
+public class TribunalService implements ITribunalService{
+	
+	@Autowired
+	public ITribunalRepository repo;
+
+	@Override
+	public Tribunal saveOrUpdate(Tribunal t) {
+		
+		return repo.save(t);
+	}
+
+	@Override
+	public void delete(Long id) {
+		repo.deleteById(id);
+		
+	}
+
+	@Override
+	public Tribunal getById(Long id) {
+
+		return repo.getOne(id);
+	}
+
+	@Override
+	public List<Tribunal> getAll() {
+		repo.findAll();
+		return null;
+	}
+
+}
---
+++ gestion-contentieux/src/main/java/com/formation/webservice/TribunalWebservice.java
@@ -0,0 +1,49 @@
+package com.formation.webservice;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formation.entities.Tribunal;
+import com.formation.service.ITribunalService;
+
+@RestController
+@RequestMapping("/apiTribunal")
+@CrossOrigin("*")
+public class TribunalWebservice {
+
+	@Autowired
+	public ITribunalService service;
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public Tribunal ajout(@RequestBody Tribunal t) {
+		return service.saveOrUpdate(t);
+	}
+	
+	@RequestMapping(value="/update", method=RequestMethod.PUT)
+	public Tribunal update(@RequestBody Tribunal t) {
+		return service.saveOrUpdate(t);
+	}
+	
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.DELETE)
+	public void delete(@PathVariable Long id) {
+		service.delete(id);
+	}
+	
+	@RequestMapping(value="/role/{id}", method=RequestMethod.GET)
+	public Tribunal getById(Long id) {
+		return service.getById(id);
+	}
+	
+	@RequestMapping(value="/roles", method=RequestMethod.GET)
+	public List<Tribunal> getAll(){
+		return service.getAll();
+	}
+	
+}
