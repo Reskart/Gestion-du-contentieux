deb9a5a218272b70085718e6b81183a235e426ca
Reskart <antoinebertinb@gmail.com>
2020-09-15 14:04:02 +0200

    ajout role webservice

--- gestion-contentieux/src/main/java/com/formation/webservice/RoleWebservice.java
+++ gestion-contentieux/src/main/java/com/formation/webservice/RoleWebservice.java
@@ -1,5 +1,28 @@
 package com.formation.webservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formation.entities.Role;
+import com.formation.service.IRoleService;
+
+@RestController
+@RequestMapping("/apiRole")
+@CrossOrigin("*")
 public class RoleWebservice {
 
+	@Autowired
+	public IRoleService service;
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public Role ajout(@RequestBody Role r) {
+		return service.saveOrUpdate(r);
+	}
+	
+	
+	
 }
