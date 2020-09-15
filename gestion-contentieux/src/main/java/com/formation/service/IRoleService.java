64199ea759dfc0170e9a28a2b9b963ce7f1837f8
Reskart <antoinebertinb@gmail.com>
2020-09-15 13:42:04 +0200

    modele service

--- gestion-contentieux/src/main/java/com/formation/service/IRoleService.java
+++ gestion-contentieux/src/main/java/com/formation/service/IRoleService.java
@@ -1,5 +1,15 @@
 package com.formation.service;
 
+import java.util.List;
+
+import com.formation.entities.Role;
+
 public interface IRoleService {
+	
+	public Role saveOrUpdate(Role r);
+	public void delete(Long id);
+	public Role getById(Long Id);
+	public List<Role> getAll();
+	
 
 }
