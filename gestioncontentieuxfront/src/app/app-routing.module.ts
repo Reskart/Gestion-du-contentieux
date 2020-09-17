c5c01d02e38209e8efcec7779e0ff3d67579cf44
MarionGl <marion.gloriant@gmail.com>
2020-09-17 14:25:53 +0200

    maj

--- gestioncontentieuxfront/src/app/app-routing.module.ts
+++ gestioncontentieuxfront/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AffaireComponent } from './affaire/affaire.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'affaire', component: AffaireComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
