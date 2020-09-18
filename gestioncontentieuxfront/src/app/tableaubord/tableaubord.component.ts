b7a37e9c128bfe857e37e43dfce85422c524edc4
Benjamin Thomas <b-thomas@outlook.fr>
2020-09-18 17:23:23 +0200

    modif tdb

--- gestioncontentieuxfront/src/app/tableaubord/tableaubord.component.ts
+++ gestioncontentieuxfront/src/app/tableaubord/tableaubord.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TDBService } from 'src/service/tdb.service';
 
 @Component({
@@ -9,14 +10,27 @@ import { TDBService } from 'src/service/tdb.service';
 })
 export class TableaubordComponent implements OnInit {
 
-  constructor(private tdbservice : TDBService, private Router : Router) { }
+  constructor(private tdbservice : TDBService, private Router: Router, private activatedRoute: ActivatedRoute) { }
   affaires:any[]=[];
-
+  form: FormGroup;
+  formValue:any;
+  statut:any;
   ngOnInit(): void {
     const myVar = false;
 
     this.tdbservice.findAll().subscribe(data =>{
       this.affaires = data as []; });
   }
+  findaffairebystatut(){
+    this.formValue=this.form.value;
+    this.statut=this.formValue['statut'];
 
+
+
+
+    this.tdbservice.getStatut(this.statut).subscribe(data=>{
+      this.statut=data;    });
+
+      this.affaires=JSON.parse(JSON.stringify(this.statut));
+  }
 }
--- gestioncontentieuxfront/src/service/tdb.service.ts
+++ gestioncontentieuxfront/src/service/tdb.service.ts
@@ -9,9 +9,15 @@ export class TDBService {
 
   URL = 'http://localhost:8080/apiAffaire'
 affaires: any[]=[];
+statut:any;
 
   constructor(private http:HttpClient) { }
   findAll() {
     return this.http.get<any[]>(this.URL+'/affaires').pipe(map(value => this.affaires=value));
   }
+
+  getStatut(statut) {
+
+    return this.http.get<any>(this.URL+'/affaireStatut/'+statut);
+}
 }
